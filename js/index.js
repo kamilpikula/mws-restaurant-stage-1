63b4d0b0051e87a59c9fc99011a4b0ab588406f6
kamilpikula <pikusix1@o2.pl>
2018-07-04 23:47:54 +0200

    add database and some keys

--- js/index.js
+++ js/index.js
@@ -9,3 +9,25 @@ if ('serviceWorker' in navigator) {
     });
   });
 }
+
+var dbPromise = idb.open('test-db', 1, function(upgradeDb) {
+   var keyValStore = upgradeDb.createObjectStore('keyval');
+   keyValStore.put('world', 'hello');
+});
+
+dbPromise.then(function(db) {
+    var tx = db.transaction('keyval');
+    var keyValStore = tx.objectStore('keyval');
+    return keyValStore.get('hello');
+}).then(function(val) {
+    console.log('The value of "hello" is:', val);
+});
+
+dbPromise.then(function(db) {
+    var tx = db.transaction('keyval', 'readwrite');
+    var keyValStore = tx.objectStore('keyval');
+    keyValStore.put('bar', 'foo');
+    return tx.complete;
+}).then(function() {
+    console.log('Added foo:bar to keyval');
+});
